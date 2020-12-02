774e761cb66fb4cbed2a8ff23f58bad6f675d13b
James Peralta <jamesperalta35@gmail.com>
2020-12-01 20:55:09 -0700

    Random Choice Working

---
+++ src/Utility/RandomChoice.java
@@ -0,0 +1,33 @@
+package Utility;
+
+public class RandomChoice{
+    private double[] prefixSums;
+    private double totalSum;
+
+    public RandomChoice(double[] w) {
+        this.prefixSums = new double[w.length];
+
+        double prefixSum = 0;
+        for (int i = 0; i < w.length; ++i) {
+            prefixSum += w[i];
+            this.prefixSums[i] = prefixSum;
+        }
+        this.totalSum = prefixSum;
+    }
+
+    public int pickIndex() {
+        double target = this.totalSum * Math.random();
+
+        // run a binary search to find the target zone
+        int low = 0, high = this.prefixSums.length;
+        while (low < high) {
+            // better to avoid the overflow
+            int mid = low + (high - low) / 2;
+            if (target > this.prefixSums[mid])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        return low;
+    }
+}
