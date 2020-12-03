f2d7fc0add95f12b81a26b721e197ab5ebc27a06
James Peralta <jamesperalta35@gmail.com>
2020-12-02 23:02:09 -0700

    Adding Next Gens

--- src/Or_Tree/GenePool.java
+++ src/Or_Tree/GenePool.java
@@ -8,28 +8,49 @@ import java.util.*;
 public class GenePool {
     private ArrayList<Prob> pool;
     private RandomChoice randomChoice;
+    private int populationSize;
+    private Department department;
 
-    public GenePool(Department department, int populationSize) {
-        pool = new ArrayList<Prob>();
-        // Generate Samples
-        for (int i = 0; i < populationSize; i++) {
-            pool.add(OrTreeBasedSearch.generateSample(department));
+    public GenePool(Department department, ArrayList<Prob> individuals, int populationSize) {
+        this.pool = individuals;
+        this.populationSize = populationSize;
+        this.department = department;
+
+        if (pool.size() == 0) {
+            for (int i = 0; i < populationSize; i++) {
+                pool.add(OrTreeBasedSearch.generateSample(department));
+            }
         }
 
-        // Sort by fitness
-        Collections.sort(pool);
-
-        // Generate random choice weight
         double[] weights = new double[populationSize];
         weights[0] = 1.0;
         for (int i = 1; i < populationSize; i++) {
             weights[i] = weights[i - 1] * 0.90;
         }
         randomChoice = new RandomChoice(weights);
+
+        // Test Individuals
     }
 
     public Prob selectRandom() {
         return pool.get(randomChoice.pickIndex());
+    }
+
+    public void testGeneration() {
+        // TODO: Need to implement testing
+    }
+
+    public void nextGeneration() {
+        ArrayList<Prob> newPool = new ArrayList<>();
+        for (int i = 0; i < this.populationSize; i++) {
+            Prob parent_one = selectRandom();
+            Prob parent_two = selectRandom();
+            Prob child = parent_one.crossover(parent_two, this.department);
+            newPool.add(child);
+        }
+
+        pool = newPool;
+        // Test individuals
     }
 
     @Override
--- src/Runner.java
+++ src/Runner.java
@@ -1,14 +1,11 @@
-import DataStructures.Course;
 import DataStructures.*;
 import Or_Tree.GenePool;
-import Or_Tree.OrTreeBasedSearch;
-
-import java.util.Collections;
+import java.util.ArrayList;
 
 public class Runner {
     public static void main(String[] args) {
         Department department = Parser.parse("./src/Search_Instances/CustomTest1.txt");
-        GenePool pool = new GenePool(department, 5);
+        GenePool pool = new GenePool(department, new ArrayList<>(), 3);
         System.out.println(pool);
     }
 }
