ab967e6455d7f869c5178b45e07cff209e4bfb79
James Peralta <jamesperalta35@gmail.com>
2020-12-03 22:11:55 -0700

    Some fixes to Eval

--- src/Utility/Eval.java
+++ src/Utility/Eval.java
@@ -1,11 +1,7 @@
 package Utility;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Set;
 
 import DataStructures.ClassElement;
 import DataStructures.CourseAssignment;
@@ -57,44 +53,18 @@ public class Eval {
     
     // 
     private int pen_section = 0;
-    
-    
-    
-   /**
-    * 
-    * @param courseSlots
-    * @param labSlot
-    * @param courses
-    * @param labs
-    * @param classElements
-    * @param department
-    */
-	Eval(ArrayList<CourseAssignment> courses,
-		 ArrayList<LabAssignment> labs,
-		 Department department){
-		// asign the things 
+
+	public Eval(ArrayList<CourseAssignment> courses, ArrayList<LabAssignment> labs, Department department){
 		this.courses = courses;
 		this.labs = labs;
 		this.department = department;
-		
-		// inialize the bound value 
 		this.bound = 0;
 		
-		
-		// check the course max and mins
+		// Check courses
 		this.checkNumOfAssigment();
-		
-
-		// professor preforence for slots 
 		this.checkPreference();
-		
-		
-		// see if courses are schdule for the same slot
-		this.checkPaired();
-		
-		// differn sections hsould be schudled at differnt times 
-		this.checkSimislarSections();
-		
+//		this.checkPaired();
+//		this.checkSimilarSections();
 	}
 	
 	// the only public 
@@ -103,109 +73,67 @@ public class Eval {
 	}
 	
 	private void checkNumOfAssigment() {
-		
-		// create a dictionary where were get the number of courses per slot 
-		HashMap<CourseSlot, Integer> coursesPer = new HashMap<CourseSlot, Integer>();
+		HashMap<CourseSlot, Integer> coursesPer = new HashMap<>();
 		for(CourseAssignment assigment: this.courses) {
-			if(coursesPer.containsKey(assigment.getCourseSlot())) {
-				// incrment the value by one 
-				coursesPer.put(
-						assigment.getCourseSlot(), 
-						coursesPer.get(assigment.getCourseSlot()) + 1
-						);
-			}else {
-				// set the key to one 
-				coursesPer.put(
-						assigment.getCourseSlot(), 
-						 1
-						);
+			CourseSlot courseSlot = assigment.getCourseSlot();
+			if (courseSlot == null) {
+				continue;
 			}
-			
+
+			if(coursesPer.containsKey(courseSlot)) {
+				coursesPer.put(courseSlot, coursesPer.get(courseSlot) + 1);
+			}
+			else {
+				coursesPer.put(courseSlot, 1);
+			}
 		}
-		
-		// now iterate though all the keys and add to the bound based on how many 
-		// are broken 
-		for(CourseSlot slot :coursesPer.keySet()) {
-			
-			
-			// check the course min and add the bound  
+		for(CourseSlot slot: coursesPer.keySet()) {
 			if(slot.getCoursemin() > coursesPer.get(slot)) {
 				this.bound += this.pen_coursemin;
 			}
-			
 		}
-		
-		
-		
-		// now check the labs 
-		HashMap<LabSlot, Integer> labsPer = new HashMap<LabSlot, Integer>();
+
+		HashMap<LabSlot, Integer> labsPer = new HashMap<>();
 		for(LabAssignment assigment: this.labs) {
-			if(labsPer.containsKey(assigment.getLabSlot())) {
-				labsPer.put(
-						assigment.getLabSlot(),
-						labsPer.get(assigment.getLabSlot()) + 1
-						);
-			}else {
-				labsPer.put(
-						assigment.getLabSlot(),
-						 1
-						);
+			LabSlot labSlot = assigment.getLabSlot();
+			if (labSlot == null) {
+				continue;
+			}
+
+			if(labsPer.containsKey(labSlot)) {
+				labsPer.put(labSlot, labsPer.get(labSlot) + 1);
+			}
+			else {
+				labsPer.put(labSlot, 1);
 			}
 		}
-		
-		// now see if they reach the lab min 
 		for(LabSlot slot : labsPer.keySet()) {
 			if(slot.getCoursemin() > labsPer.get(slot)) {
 				this.bound += this.pen_labsmin;
 			}
 		}
 	}
-	/**
-	 * Certain professors that often teach certain courses have certain preferences regarding in 
-	 * which time slots their courses and labs should be scheduled. Naturally, we see this as something 
-	 * that should be treated as soft constraint. Depending on a to-be-determined ranking scheme, each 
-	 * professor will be awarded a certain set of ranking points and he/she can distribute these points over 
-	 * pairs of (course/lab, time slots). Formally, we assume a function preference: 
-	 * (Courses + Labs) x Slots -> Natural numbers that reports those preferences.
-	 */
+
 	private void checkPreference() {
-		
-		// okay so the goal of this is to maximixe the score of each 
-		
-		
-		// so when the preference is not the highest score avalible there is a penalty added 
-		
-			// the delta from the top will be a higher penalty 
-		
-		
-		
-		// probably the first thing im gonna want to do is create a map of courses 
-		// that maps 
-		
-		
-		// 
-		//HashMap<CourseInstance,HashMap<Slot,Integer>> coursePreferenceMap; 
 		for(CourseAssignment course: this.courses) {
 			CourseSlot slot =  course.getCourseSlot();
+
+			if (slot == null) {
+				continue;
+			}
+
 			if(course.getCourse().getPreference().containsKey(slot)) {
-				// for each course just check to see if the prefernce slot exists in the map
-				// and if it does then we should see what order the score is in
-				
-				
-				// create a list of all the scores 
-				var keys = course.getCourse().getPreference().keySet().toArray();
+				Object[] keys = course.getCourse().getPreference().keySet().toArray();
 				int[] scores = new int[keys.length];
 				for(int i =0; i<keys.length;i++) {
 					scores[i] = course.getCourse().getPreference().get(keys[i]);
 				}
-				
-				// now sort the list of scores 
-				Arrays.sort(scores );
-				
-				// reverse the array 
-				for(int i=0; i<scores.length/2; i++){
+
+				Arrays.sort(scores);
+
+				for(int i = 0; i < scores.length / 2; i++){
 		            int temp = scores[i];
-		            scores[i] = scores[scores.length -i -1];
+		            scores[i] = scores[scores.length - i -1];
 		            scores[scores.length -i -1] = temp;
 		        }
 				
@@ -232,28 +160,13 @@ public class Eval {
 				// add the penalty, for now the penalty is caluclated more so 
 				// we can easly change this in a function above 
 				this.bound += this.pen_prefernce_calculated(position,scores.length);
-				
-				
-				
 			}
-			
-			
-			
 		}
 		
 		
 	}
-	
-	/**
-	 * TODO need to make sure that it does no 
-	 */
+
 	private void checkPaired() {
-		
-		// we will get a list of pairs 
-		// for each class see if it has a pair then see if the pair is at the same slot
-		
-		
-		
 		for(CourseAssignment courseAssigment: this.courses) {
 			// check to see if there are classes that should be compadible to this  
 			 
@@ -262,14 +175,10 @@ public class Eval {
 				for(ClassElement pair:  courseAssigment.getCourse().getCompatible()) {
 					// need to find where it is assigned 
 					Slot pairSlot = this.assigments.get(pair);
-					
-					
-					
-					// then we can check it to the assugment slot 
-					
+
+					// then we can check it to the assugment slot
 					Slot masterSlot =  courseAssigment.getCurrentSlot();
-					
-					
+
 					// then see if the 2 slots are the same 
 					boolean dayMatch = pairSlot.getDayString() == masterSlot.getDayString();
 					boolean timeMatch = pairSlot.getTimeString() == masterSlot.getTimeString();
@@ -280,11 +189,7 @@ public class Eval {
 						// the pair occurs on a differnt time slot 
 						this.bound += this.pen_pair;
 					}
-					
-					
-					
 				}
-//				
 			}
 		}
 		// we need to loop though the list
@@ -312,28 +217,13 @@ public class Eval {
 						this.bound += this.pen_pair;
 						
 					}
-					
-					
 				}
 			}
 				
 		}
-		
-		
 	}
-	/**
-	 * Different sections of a course should be scheduled at different times. 
-	 * For each pair of sections that is scheduled into the same slot, we add a penalty 
-	 * pen_section to the Eval-value of an assignment assign.
-	 */
-	private void checkSimislarSections() {
-		
-		// example we have l01, l02, l03
-		
-		/**
-		 * say that l01 and l02 are at the same time 
-		 * then we give a penalty for l01 and l02 but not when checking l02 to l02
-		 */
+
+	private void checkSimilarSections() {
 		for(String course: this.department.getCourseMap().keySet()) {
 			ArrayList<CourseInstance> courseSet = this.department.getCourseMap().get(course);
 			// check all the courses 
@@ -373,9 +263,7 @@ public class Eval {
 			}
 			
 		}
-		
-		
-		
+
 		// now lets check all of the labs together 
 		for(String lab: this.department.getLabMap().keySet()) {
 			ArrayList<LabSection> labSet = this.department.getLabMap().get(lab);
@@ -405,17 +293,10 @@ public class Eval {
 								this.bound += this.pen_section;
 							}
 						}
-						
-						
 					}
 				}
 			}
-			
-			
 		}
-		
-		
-		
 	}
 	
 	
