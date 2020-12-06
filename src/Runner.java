755687e8b80b48dd0cbc5f3feb56b858b9530176
James Peralta <jamesperalta35@gmail.com>
2020-12-06 11:27:38 -0700

    New changes

--- src/Runner.java
+++ src/Runner.java
@@ -10,7 +10,7 @@ public class Runner {
     private static int populationSize = 50;
 
     public static void main(String[] args) {
-        Department department = Parser.parse("./src/Abel_Instances/913.txt");
+        Department department = Parser.parse("./src/Abel_Instances/Test1.txt");
         GenePool pool = new GenePool(department, new ArrayList<>(), populationSize);
         Prob bestIndividual = pool.getBestAssignment();
         for (int i = 0; i < generations; i++) {
